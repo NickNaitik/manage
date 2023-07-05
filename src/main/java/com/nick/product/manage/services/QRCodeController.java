92b1225dbad87cb42e30b226d3a4eac1fee816d5
NickNaitik <naitikyadav110@gmail.com>
2023-07-05 11:10:03 -0700

    QR logic completed.

---
+++ src/main/java/com/nick/product/manage/services/QRCodeController.java
@@ -0,0 +1,39 @@
+package com.nick.product.manage.services;
+
+import com.google.zxing.WriterException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("api/v1/qrcode")
+public class QRCodeController {
+
+    @Autowired
+    private QRCodeGenerator qrCodeGenerator;
+
+    @GetMapping(value = "/{amount}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateQRCodeImage(
+            @PathVariable("amount") String amount
+    ) throws WriterException, IOException {
+        String qrText = "upi://pay?pa=" + "8757394648@ybl" + "&pn=Your%20Name&mc=1234&tid=123456&tr=0123456789&tn=Payment%20Description&am=" + amount;
+
+        int width = 300;
+        int height = 300;
+
+        byte[] qrCodeImage = qrCodeGenerator.generateQRCodeImage(qrText, width, height);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return new ResponseEntity<>(qrCodeImage, headers, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/nick/product/manage/services/QRCodeGenerator.java
@@ -0,0 +1,28 @@
+package com.nick.product.manage.services;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QRCodeGenerator {
+
+    public byte[] generateQRCodeImage(String qrText, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(qrText, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", byteArrayOutputStream);
+
+        return byteArrayOutputStream.toByteArray();
+    }
+}
